681c094f5b8a9a7f26620d4d2ea168fcf0b6aed0
siri.svedberg.0114 <siri.svedberg.0114@student.lu.se>
2023-09-11 16:41:22 +0200

    assessment 2 är klar

--- labs/lab1/lab1.mjs
+++ labs/lab1/lab1.mjs
@@ -1,7 +1,9 @@
 'use strict';
 /**
  * Reflection question 1
- * your answer goes here
+ * för att JS är ett funktionellt språk
+ * det är skillnad på "om den finns" eller om den har ett värde.
+ * 
  */
 
 import inventory from './inventory.mjs';
@@ -20,23 +22,83 @@ for (const name in inventory) {
 }
 /**
  * Reflection question 2
+ * when will the two examples above give different outputs, and why
+is inherited functions, such as forEach(), not printed? Hint: read about enumerable
+properties and own properties.
  */
 
 console.log('\n--- Assignment 1 ---------------------------------------')
 
 function makeOptions(inv, prop) {
-  return 'TODO';
+  if (!Array.isArray(inv)) {
+    // Handle the case where inv is not an array, e.g., return an empty string
+    return "inv är inte en array lol";
+  }
+  const filteredIngredients = inv.filter((ingredient) => ingredient[prop]);
+  
+  const optionsHTML = filteredIngredients.map((ingredient) => {
+    const { name, price } = ingredient;
+    const key = name; // Set the key attribute using the name
+    return `<option value="${name}" key="${key}">${name}, ${price} kr</option>`;
+  });
+
+  return optionsHTML.reduce((options, option) => options + option, "");
 }
+ 
 
 console.log(makeOptions(inventory, 'foundation'));
 
 console.log('\n--- Assignment 2 ---------------------------------------')
 class Salad {
-  constructor() { }
-  add(name, properties) { }
-  remove(name) { }
+  constructor() { 
+    this.foundation = null;
+    this.protein = [];
+    this.extra = [];
+    this.dressing = [];
+  }
+  add(name, properties) {
+    if (name === "foundation"){
+      if (!this.foundation) {
+        this.foundation=properties;
+      } else {
+        console.log('Error: bara 1 foundation bror');
+        return;
+      }
+    } else if (name === "protein"){
+      this.protein.push(properties);
+    } else if (name === "extra"){
+      this.extra.push(properties);
+    } else if (name === "dressing"){
+      this.dressing.push(properties);
+    }
+    console.log( name + ' added');
+    return this;
+   }
+  remove(name) { 
+    if (name === "foundation") {
+      this.foundation=null;
+    } else if (name === "protein") {
+      this.protein.splice(this.protein.indexOf(name), 1);
+    } else if (name === "extra") {
+      this.extra.splice(this.extra.indexOf(name), 1);
+    } else if (name === "dressing") {
+      this.dressing.splice(this.dressing.indexOf(name), 1);
+    }
+    console.log(name + ' removed!');
+    return this;
+  }
+ /**  toJSON() {
+    return { "ingredients": this.ingredients };
+  }
+  */
+  getPrice(){
+    return 70;
+  }
+  count(properties){
+    return 70;
+  }
 }
-/*
+
 let myCaesarSalad = new Salad()
   .add('Sallad', inventory['Sallad'])
   .add('Kycklingfilé', inventory['Kycklingfilé'])
@@ -48,11 +110,14 @@ let myCaesarSalad = new Salad()
 console.log(JSON.stringify(myCaesarSalad) + '\n');
 myCaesarSalad.remove('Gurka');
 console.log(JSON.stringify(myCaesarSalad) + '\n');
-*/
+
 console.log('\n--- Assignment 3 ---------------------------------------')
-//console.log('En ceasarsallad kostar ' + myCaesarSalad.getPrice() + 'kr');
+
+
+
+console.log('En ceasarsallad kostar ' + myCaesarSalad.getPrice() + 'kr');
 // En ceasarsallad kostar 45kr
-//console.log('En ceasarsallad har ' + myCaesarSalad.count('lactose') + ' ingredienser med laktos');
+console.log('En ceasarsallad har ' + myCaesarSalad.count('lactose') + ' ingredienser med laktos');
 // En ceasarsallad har 2 ingredienser med laktos
 //console.log('En ceasarsallad har ' + myCaesarSalad.count('extra') + ' tillbehör');
 // En ceasarsallad har 3 tillbehör
